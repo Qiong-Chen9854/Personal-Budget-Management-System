9d03d72d4f0c0de5ad3dfb729c0e7f93946f049e
Qiong Chen <chenqiong.chen@gmail.com>
2024-11-12 14:48:52 -0500

    registed user from client-side

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -1,45 +1,78 @@
 BEGIN TRANSACTION;
---ROLLBACK;
-DROP TABLE IF EXISTS budgets, expenses,income, accounts,users;
+-- ROLLBACK;
+DROP TABLE IF EXISTS budgets, expenses,incomes, account,tenmo_user;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id, seq_income_id,seq_expense_id,seq_budget_id;
 
-CREATE TABLE users(
-	user_id SERIAL PRIMARY KEY,
-	username varchar(200) NOT NULL,
+CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  START WITH 1001
+  NO MAXVALUE;
+
+CREATE TABLE tenmo_user (
+	user_id int NOT NULL DEFAULT nextval('seq_user_id'),
+	username varchar(50) UNIQUE NOT NULL,
 	password_hash varchar(200) NOT NULL,
-	role varchar(50),	
+	role varchar(20),
+	CONSTRAINT PK_tenmo_user PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username)
 );
 
-CREATE TABLE accounts(
-	account_id serial PRIMARY KEY,
-	user_id int,
-	 balance NUMERIC(12, 2) DEFAULT 100.00,
-	CONSTRAINT fk_accounts_user_id foreign key (user_id) references users(user_id)
+CREATE SEQUENCE seq_account_id
+  INCREMENT BY 1
+  START WITH 2001
+  NO MAXVALUE;
+
+CREATE TABLE account (
+	account_id int NOT NULL DEFAULT nextval('seq_account_id'),
+	user_id int NOT NULL,
+	balance decimal(13, 2) NOT NULL,
+	CONSTRAINT PK_account PRIMARY KEY (account_id),
+	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
 
+CREATE SEQUENCE seq_income_id
+	INCREMENT BY 1
+	START WITH 3001
+	NO MAXVALUE;
+	
 CREATE TABLE incomes(
-	income_id serial PRIMARY KEY,
+	income_id int not null default nextval('seq_income_id'),
 	user_id int,
 	amount numeric(12,2) default 0.00,
 	source varchar(225),
 	date DATE default current_date,
-	CONSTRAINT fk_incomes_user_id foreign key (user_id) references users(user_id)
+	CONSTRAINT pk_incomes PRIMARY KEY(income_id),
+	CONSTRAINT fk_incomes foreign key (user_id) references tenmo_user(user_id)
 );
 
+CREATE SEQUENCE seq_expense_id
+	INCREMENT BY 1
+	START WITH 4001
+	NO MAXVALUE;
+	
 CREATE TABLE expenses(
-	expense_id serial PRIMARY KEY,
+	expense_id int not null default nextval('seq_expense_id'),
 	user_id int,
 	amount numeric(12,2) default 0.00,
 	category varchar(225),
-	date DATE default current_date
+	date DATE default current_date,
+	CONSTRAINT pk_expenses PRIMARY KEY (expense_id),
+	CONSTRAINT fk_expenses FOREIGN KEY (user_id) REFERENCES tenmo_user(user_id)
 );
+
+CREATE SEQUENCE seq_budget_id
+	INCREMENT BY 1
+	START WITH 5001
+	NO MAXVALUE;
 
 CREATE TABLE budgets(
-	budget_id serial PRIMARY KEY,
+	budget_id int not null default nextval('seq_budget_id'),
 	user_id int,
 	amount numeric(12,2) default 0.00,
 	category varchar(225),
-	month_year date default null
+	month_year date default null,
+	CONSTRAINT pk_budgets PRIMARY KEY (budget_id),
+	CONSTRAINT fk_budgets FOREIGN KEY (user_id) REFERENCES tenmo_user(user_id)
 );
 
 COMMIT;
